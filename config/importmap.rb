2926a83449a2b32633f84299c166bbec000a8d24
tsu-na-gu <106959506+tsu-na-gu@users.noreply.github.com>
2024-01-30 11:09:43 +0900

    Delete config/importmap.rb

--- config/importmap.rb
+++
@@ -1,9 +0,0 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "comments", to: "comments.js"
-pin "remove_flash_message", to: "removals_controller.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
